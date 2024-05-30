7f555e5f7d5b0f662ef98303e2f13b1ce7754e06
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2024-05-30 09:28:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
+var allcontrollers = [
+    {
+        company: "sony",
+        type: "PS5",
+        price: "5000",
+    },
+    {
+        company: "sony",
+        type: "PS4",
+        price: "4000",
+    },
+    {
+        company: "Microsoft",
+        type: "XBOX1",
+        price: "3000",
+    },
+];
 
-  var allcontrollers = [
-    {
-      company: "sony",
-      type: "PS5",
-      price: "5000",
-    },
-    {
-      company: "sony",
-      type: "PS4",
-      price: "4000",
-    },
-    {
-      company: "Microsoft",
-      type: "XBOX1",
-      price: "3000",
-    },
-  ];
-  
-  var controllerDiv = document.getElementById("controllers");
-  
-  function getItem(ele) {
+var controllerDiv = document.getElementById("controllers");
+
+function getItem(ele) {
     console.log("element==>", ele.id);
 
-    allcontrollers.splice(ele.id, 1);
+    // Correcting the deletion logic
+    var index = parseInt(ele.id); // Parsing ID to integer
+    allcontrollers.splice(index, 1);
     console.log("allcontrollers updated=>", allcontrollers);
     showAllData();
-  }
-  
-  function showAllData() {
+}
+
+function showAllData() {
     controllerDiv.innerHTML = "";
     allcontrollers.forEach(function (data, ind) {
-      var ele = `<div onclick = "getItem(this)" id=${ind}>${data.code} - ${data.company} - ${data.type}  </div>`;
-  
-      controllerDiv.innerHTML += ele;
+        // Fixing the display of controller data
+        var ele = `<div onclick="getItem(this)" id="${ind}">${data.price} - ${data.company} - ${data.type}</div>`;
+        controllerDiv.innerHTML += ele;
     });
-  }
+}
+
+// Ensure initial data is shown
+showAllData();
+
   
